e32095f79f0bf1c5e2eb123903a54745ad37d207
Raj Shekhar <rajshekhar8926@gmail.com>
2021-06-17 18:51:17 +0530

    Add course-info

---
+++ part1/course-info/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Part = ({ part, exercise }) => (
+	<p>
+		{part} {exercise}
+	</p>
+);
+
+const Content = ({ parts }) => {
+	return (
+		<>
+			<Part part={parts[0].name} exercise={parts[0].exercises} />
+			<Part part={parts[1].name} exercise={parts[1].exercises} />
+			<Part part={parts[2].name} exercise={parts[2].exercises} />
+		</>
+	);
+};
+
+const Total = ({ parts }) => {
+	return (
+		<p>
+			Number of exercises{' '}
+			{parts[0].exercises + parts[1].exercises + parts[2].exercises}
+		</p>
+	);
+};
+
+const App = () => {
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+			{
+				name: 'Fundamentals of React',
+				exercises: 10,
+			},
+			{
+				name: 'Using props to pass data',
+				exercises: 7,
+			},
+			{
+				name: 'State of a component',
+				exercises: 14,
+			},
+		],
+	};
+
+	return (
+		<div>
+			<Header name={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	);
+};
+
+export default App;
